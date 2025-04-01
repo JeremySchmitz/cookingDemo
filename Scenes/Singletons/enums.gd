74ab7476e293f89671e51bd04a1753aa0ae5b1cb
Jeremy Schmitz <schmitz.jers@gmail.com>
2025-04-01 19:09:39 -0400

    Added Crew Class and Scene

--- Scenes/Singletons/enums.gd
+++ Scenes/Singletons/enums.gd
@@ -1,3 +1,4 @@
 class_name GlobalEnums
 enum Mode {CHOP, SLICE, GRAB}
 enum Cooked {RAW, MEDIUM, WELL, BURNT}
+enum role {CAPTAIN, FIRSTMATE, CREW}
---
+++ scripts/crewmate.gd
@@ -0,0 +1,8 @@
+class_name Crew
+
+@export var role:= GlobalEnums.role.CREW
+@export var health: int = 100
+#How Susceptable they are poisoned food
+@export var constitution: int = 100
+@export var strength: int = 100
+@export var fishing: int = 100
