b15b568ccff29616e76861fd658113f2a49be0eb
Jeremy Schmitz <schmitz.jers@gmail.com>
2025-07-30 18:58:53 -0400

    Added Bones that can be cut through

---
+++ scripts/Food/bone.gd
@@ -0,0 +1,2 @@
+class_name Bone
+extends Organ
--- scripts/knives/knife_chopper.gd
+++ scripts/knives/knife_chopper.gd
@@ -1,12 +1,17 @@
 class_name KnifeChopper
 extends Knife
 
+@export var boneParticles: CPUParticles2D
+
 var active: bool = false
 var cutting: bool = false
 
 
+func _ready():
+	super._ready()
+
 func _process(_delta):
-	if(active):
+	if (active):
 		global_position = get_global_mouse_position()
 		if inCuttingBoard && Input.is_action_just_pressed("left_click"):
 			cutting = true
@@ -21,3 +26,11 @@ func _on_mode_change(m: GlobalEnums.Mode):
 			active = true
 		_:
 			active = false
+
+func _on_area_entered(body: Area2D):
+	super._on_area_entered(body)
+	if cutting && body.get_parent() is Bone:
+		cutting = false
+		if boneParticles:
+			boneParticles.global_position = body.global_position
+			boneParticles.emitting = true
