86f2d9caf80fdda9fc8255488c1772140b02cc76
Jeremy Schmitz <schmitz.jers@gmail.com>
2025-08-11 20:54:27 -0400

    cleanup because I missed things last commit

--- scripts/story/boat_char.gd
+++ scripts/story/boat_char.gd
@@ -1,27 +1,7 @@
 extends CharacterBody2D
 class_name BoatChar
 
-
-signal moving(val: bool)
-signal nextPosition(val: Vector2)
-
 @export var speed := 100.0
-@onready var navigation: NavigationAgent2D = $NavigationAgent2D
-@onready var maxDist = navigation.target_desired_distance ** 2
-
-var navPathDist := 0.0
-var targetReachedBool = false;
-var nextPathPos: Vector2
-var isMoving = false:
-	set(val):
-		isMoving = val
-		moving.emit(val)
-
-func _ready() -> void:
-	isMoving = false
-
-@export var friction = 0.01
-@export var acceleration = 0.1
 
 
 func _input(event: InputEvent) -> void:
@@ -31,8 +11,3 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta):
 	look_at(global_position + velocity.rotated(-PI / 2))
 	move_and_slide()
-
-func setTargetPos(pos: Vector2):
-	navigation.target_position = pos
-	targetReachedBool = false
-	isMoving = true
