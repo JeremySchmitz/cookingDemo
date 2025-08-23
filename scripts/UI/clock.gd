e6fbbee311951e95920a3fe9340cfead0d58322b
Jeremy Schmitz <schmitz.jers@gmail.com>
2025-08-22 21:10:53 -0400

    Created Clock Component

---
+++ scripts/UI/clock.gd
@@ -0,0 +1,36 @@
+extends Control
+class_name Clock
+
+signal done()
+
+@export var duration: float
+@export var timeScale := 1.0
+@onready var sprite: AnimatedSprite2D = $%AnimatedSprite2D
+@onready var frameCount: int = sprite.sprite_frames.get_frame_count('default')
+var started = false
+var timeLeft: float
+var time := 0.0
+func _ready() -> void:
+	start()
+
+
+func _process(delta: float) -> void:
+	if started:
+		time += delta * timeScale
+		_updateAnimation()
+		if time >= duration: _done()
+
+func start():
+	started = true
+
+func pause():
+	started = false
+
+func _done():
+	started = false
+	time = 0.0
+	done.emit()
+
+func _updateAnimation():
+	var frame = remap(time, 0, duration, 0, frameCount)
+	sprite.frame = floor(frame)
